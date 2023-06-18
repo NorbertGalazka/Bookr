4e4ab67c516d5b5b3d1fa0c5bd9ed331d05ad63c
Norbert Galazka <norbert1996galazka@gmail.com>
2023-06-18 15:44:00 +0200

    add a new views function

--- reviews/views.py
+++ reviews/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Book
 from .utils import avarage_rating
@@ -6,17 +6,35 @@ from .utils import avarage_rating
 
 def book_list(request):
     books = Book.objects.all()
-    book_list = []
+    books_list = []
     for book in books:
         reviews = book.review_set.all()
         if reviews:
-            book_rating = avarage_rating([review.rating for review in reviews])
-            number_of_reviews = len(reviews)
+            avarege_rating = avarage_rating([review.rating for review in reviews])
+            num_of_reviews = len(reviews)
         else:
-            book_rating = None
-            number_of_reviews = 0
-        book_list.append({'book': book, 'book_rating': book_rating, 'number_of_reviews': number_of_reviews})
+            avarege_rating = None
+            num_of_reviews = 0
 
-        context = {'book_list': book_list}
+        dict = {'book_object': book, 'avarege_rating': avarege_rating, 'num_of_reviews': num_of_reviews}
+        books_list.append(dict)
 
-    return render(request, 'C:\\Users\\Norbert\\PycharmProjects\\Bookr\\reviews\\templates\\book_list.html', context)
+    dict_to_html = {'books': books_list}
+
+    return render(request, 'C:\\Users\\Norbert\\PycharmProjects\\Bookr\\reviews\\templates\\book_list.html', dict_to_html)
+
+
+def book_detail(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    reviews = book.review_set.all()
+    if reviews:
+        book_rating = avarage_rating([review.rating for review in reviews])
+        context = {"book": book, "book_rating": book_rating, "reviews":reviews}
+
+    else:
+        context ={"book": book, "book_rating": None, "reviews":None}
+
+    return render(request, "C:\\Users\\Norbert\\PycharmProjects\\Bookr\\reviews\\templates\\book_detail.html", context)
+
+
+
