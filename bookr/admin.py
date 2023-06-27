4946cf4ee319a10821241776f3948dc7912c75b9
Norbert Galazka <norbert1996galazka@gmail.com>
2023-06-27 11:14:40 +0200

    changing the name of the project

---
+++ bookr/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+class BookrAdminSite(admin.AdminSite):
+    title_header = 'Aplikacja administracyjna bookr'
+    site_header = 'Aplikacja administracyjna bookr'
+    index_title = 'Aplikacja administracyjna bookr'
